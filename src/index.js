e06ef829d051f00785ea342b41fb4a1e81181637
ndkhanhs <mainver01@gmail.com>
2023-09-13 20:41:43 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.listen("3000", () => {
+     console.log("bot is running");
+});
+
+const Discord = require("discord.js");
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Discord.Client({
+     intents : [
+          GatewayIntentBits.MessageContent,
+          GatewayIntentBits.GuildMessages,
+          GatewayIntentBits.Guilds
+     ]
+});
+
+client.on("ready", () => {
+     console.log("Im ready !!");
+})
+
+client.on("messageCreate", (msg) => {
+     if(msg.content === "ping")
+          msg.reply("what name!!");
+});
+
+client.on("interactionCreate", (interaction) => {
+     if(!interaction.isChatInputCommand()) return;
+     var commandName = interaction.commandName
+     switch(commandName){
+          case "hey":
+               interaction.reply("hey boy");
+               break;
+          case "sayhello":
+               interaction.reply(`Hello`);
+               break;
+     }
+});
+
+client.login(process.env.TOKEN);
---
+++ src/register_command.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const {REST, Routes} = require("discord.js");
+const commands = [
+     {
+          name: "hey",
+          description: "Hello my boy",
+     },
+     {
+          name: "sayhello",
+          description: "Hello my boy",
+     },
+];
+
+const tilte_regist = "[REGIST]";
+const tilte_err = "[ERROR]";
+const rest = new REST({version: "10"}).setToken(process.env.TOKEN);
+(async () => {
+     try {
+          console.log(`${tilte_regist}: in progress...`);
+          await rest.put(
+               Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID)
+               ,{body : commands}
+          )
+          console.log(`${tilte_regist}: done!`);
+     } catch (error) {
+          console.log(`${tilte_err}: ${error}`);
+     }
+})();
