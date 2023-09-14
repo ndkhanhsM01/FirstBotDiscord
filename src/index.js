8d8d242021f8f1bfbdcf4eeadbcfb801bf7fd5f1
ndkhanhs_m01 <142150794+ndkhanhsM01@users.noreply.github.com>
2023-09-14 11:29:26 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,8 @@ client.on("messageCreate", (msg) => {
           msg.reply("what name!!");
 });
 
-client.on("interactionCreate", (interaction) => {
+client.on("interactionCreate", async (interaction) => {
+     await interaction.deferReply();
      if(!interaction.isChatInputCommand()) return;
      var commandName = interaction.commandName
      switch(commandName){
